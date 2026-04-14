23e3970f142e22da82062f20e159532e1906a124
Callisto95 <luluklzde@gmail.com>
2026-04-14 17:15:54 +0200

    add algebra:base_change

--- algebra.py
+++ algebra.py
@@ -1,3 +1,5 @@
+from math import ceil, log10
+
 from tu_bs_scripts.quick_cli import quick_run, cli
 
 
@@ -22,6 +24,36 @@ def euklid_modern(a: int, b: int) -> int:
         b = h
         print(f"a: {a}, b: {b}, mod {h}")
     return a
+
+
+@cli
+def base_change(number: str | int, source_base: int, target_base: int) -> str:
+    values: list[int] = []
+    
+    rest: int = int(str(number), source_base)
+    if source_base != 10:
+        print(f"necessary conversion: ({number}){source_base} to ({rest})10")
+    
+    # alignment helpers
+    length_rest: int = ceil(log10(rest))
+    length_target_base: int = ceil(log10(target_base))
+    
+    while rest != 0:
+        rest_save: int = rest
+        rest, mod = divmod(rest, target_base)
+        print(f"{rest_save:>{length_rest}} / {target_base} = {rest:>{length_rest}} % {mod:>{length_target_base}}")
+        values.append(mod)
+    
+    values.reverse()
+    if target_base < 10:
+        # a number can be created nicely
+        return "".join([str(value) for value in values])
+    elif target_base == 16:
+        # hex returns '0x...', so it must be stripped
+        return "0x" + "".join([hex(value)[2:].upper() for value in values])
+    else:
+        # fallback: just dump the array
+        return repr(values)
 
 
 if __name__ == '__main__':
